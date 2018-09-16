0a3dae107e955dee4cc7c1c929ab873876dce0df
GauPat <patrick.gauls@web.de>
2018-09-16 22:13:43 +0200

    Feature: Home - added Overview for rental offers

--- home/views.py
+++ home/views.py
@@ -1,27 +1,9 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
 from django.utils import translation
+
 import os
 
 
-# Create your views here.
 class HomeTemplateView(TemplateView):
     template_name = "home/home.html"
-    
-    def dispatch(self, request, *args, **kwargs):
-#         print(request.LANGUAGE_CODE)
-#         request.session[translation.LANGUAGE_SESSION_KEY] = request.POST.get('language')
-#         if request.session and 'language' in request.session:
-#             print(str(request.session.get('language', "sessin: NOT SET")))
-#         else:
-#             print("sessin: NOT SET")
-#         print(request.session[translation.LANGUAGE_SESSION_KEY])
-#         print(str(request.COOKIES.get('language',  "cookie: NOT SET")))
-#         
-#         print("Header: " + request.META['HTTP_ACCEPT_LANGUAGE'])
-# 
-#         if 'HTTP_ACCEPT_LANGUAGE' in request.META:
-#             del request.META['HTTP_ACCEPT_LANGUAGE']
-            
-        # translation.activate('en')
-        return TemplateView.dispatch(self, request, *args, **kwargs)
