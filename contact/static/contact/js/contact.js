87363a58e5bb2c3035abc44ead089c906d1f1104
GauPat <patrick.gauls@web.de>
2018-09-21 02:02:48 +0200

    Feature: contact app - add contact form site and location and approach site

---
+++ contact/static/contact/js/contact.js
@@ -0,0 +1,29 @@
+/* exported initContatLocationAndApproach */
+
+/**
+ * Initializes a regular google map.
+ */
+function regularMap() {
+    var varLocation = new google.maps.LatLng(50.9780463, 6.8841171);
+
+    var varMapoptions = {
+        center: varLocation,
+        zoom: 14
+    };
+
+    var varMap = new google.maps.Map(document.getElementById("map-container-5"),
+        varMapoptions);
+
+    var varMarker = new google.maps.Marker({
+        position: varLocation,
+        map: varMap,
+        title: "Butzweilerhof"
+    });
+}
+
+/**
+ * init functionn to set up the google map.
+ */
+function initContatLocationAndApproach() {
+	google.maps.event.addDomListener(window, 'load', regularMap);
+}
